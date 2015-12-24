b78ab4858e0c84c0689c1ac997643b5f397c990b
thinkingCat <zjforit@gmail.com>
2015-12-24 17:51:33 +0800

    add similarity function

---
+++ MakingRecommendations/py/recommendations.py
@@ -0,0 +1,9 @@
+from math import sqrt
+
+def similarity(item, person_1, person_2):
+    common_item = {e for e in item[person_1] if e in item[person_2]}
+    if len(common_item) == 0:
+        return 0
+    distance = sqrt(sum([pow(item[person_1][m] - item[person_2][m], 2) for m in common_item]))
+    return 1 / (1 + distance)
+    
