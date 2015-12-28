48b6ff90eecc67c99f2c35ad81dd6881c3545fd7
thinkingCat <zjforit@gmail.com>
2015-12-28 17:20:15 +0800

    add pearson distance

--- MakingRecommendations/py/recommendations.py
+++ MakingRecommendations/py/recommendations.py
@@ -8,5 +8,27 @@ def euclidean_distance(item, person_1, person_2):
     return 1 / (1 + distance)
     
 def pearson_distance(item, person_1, person_2):
-    common_item = {}
+    common_item = [e for e in item[person_1] if e in item[person_2]]
+    if len(common_item) == 0:
+        return 0
+    
+    # 算术平均数
+    p1 = [item[person_1][m] for m in common_item]
+    p2 = [item[person_2][m] for m in common_item]
+    length = len(common_item)
+    p1_mean = sum(p1) / length
+    p2_mean = sum(p2) / length
+    
+    # 向量减去算术平均数
+    p_1 = map(lambda x : x - p1_mean, p1)
+    p_2 = map(lambda x : x - p2_mean, p2)
+    
+    #计算 p_1 和 p_2 的余弦距离
+    p_1_2_dot = sum([p_1[i] * p_2[i] for i in xrange(len(p_1))])
+    p_1_mo = sqrt(sum(e**2 for e in p_1))
+    p_2_mo = sqrt(sum(e**2 for e in p_2))
+    
+    if p_1_mo == 0 || p_2_mo == 0:
+        return 0
+    return p_1_2_dot / (p_1_mo * p_2_mo)
     
