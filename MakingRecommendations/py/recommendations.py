40cf2f7cfae71b14ca43d178347ff54216ffb7a2
thinkingCat <zjforit@gmail.com>
2015-12-28 21:19:02 +0800

    add item-based recommend

--- MakingRecommendations/py/recommendations.py
+++ MakingRecommendations/py/recommendations.py
@@ -32,7 +32,45 @@ def pearson_distance(item, elem_1, elem_2):
         return 0
     return p_1_2_dot / (p_1_mo * p_2_mo)
     
-def topMatches(item, elem, n = 7, similarity = pearson_distance):
+def top_matches(item, elem, n = 7, similarity = pearson_distance):
     candidates = [(other, similarity(item, elem, other)) for other in item if other != elem]
     candidates.sort(key = lambda x : x[1], reverse = True)
     return candidates[:n]
+
+def trans_item(item):
+    result = {}
+    for k1 in item:
+        for k2 in item[k1]:
+            if k2 not in result:
+                result[k2] = {}
+            result[k2][k1] = item[k1][k2]
+    return result
+ 
+def get_similar_items(item, n = 21):
+    result = {}
+    new_item = trans_item(item)
+    for e in new_item:
+        near_item = top_matches(new_item, e, n)
+        result[e] = near_item
+    return result
+    
+def item_based_recommend(item, items_table, user):
+    user_item = item[user]
+    scores = {}
+    total_sim = {}
+    
+    for (good, score) in user_item:
+        for (other_good, similarity) in items_table[good]:
+            if other_good in user_item:
+                continue
+            if other_good not in scores:
+                scores[other_good] = 0
+            scores[other_good] += score * similarity
+            if other_good not in total_sim:
+                total_sim[other_good] = 0
+            total_sim[other_good] += similarity
+     
+     # ranking
+     rank_list = [(e, float(score) / total_sim[e]) for (e, score) in scores]
+     rank_list.sort(key = lambda x : x[1], reverse = True)
+     return rank_list
